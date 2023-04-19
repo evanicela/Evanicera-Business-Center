c19ff5c4bcbeb49604945c513882e11d4294c9b8
evanicela <niyonizigiye.celestin@gmail.com>
2023-04-19 11:39:37 +0300

    first commit

---
+++ scritpt.js
@@ -0,0 +1,7 @@
+HTML CSS JSResult Skip Results Iframe
+EDIT ON
+document
+  .querySelector(".menu-btn")
+  .addEventListener("click", () =>
+    document.querySelector(".main-menu").classList.toggle("show")
+  );
